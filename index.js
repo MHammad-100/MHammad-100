e8a7aebd041a9cba464a9351f4d91299cbb7993c
MHammad-100 <84732821+MHammad-100@users.noreply.github.com>
2021-09-10 19:57:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,30 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 
 );
-function Col()
-{
-  let count =  parseInt(document.getElementById('counter').innerHTML);
-  console.log(typeof(count));
-  let body = document.getElementById('body');
-  console.log(body);
-  if(count == 20)
-  {
-    body.style.backgroundColor='black';
-  }
-  else if(count==10)
-  {
-    body.style.backgroundColor='yellow';
-  }
-  else if(count==5)
-  {
-    body.style.backgroundColor='white';
-  }
-  else
-  {
-
-  }
-}
-Col();
 // 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
